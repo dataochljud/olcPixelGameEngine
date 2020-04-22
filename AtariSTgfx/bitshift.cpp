359980507513514a79591bd50ea5ad32786f1348
Data & Ljud <johan@dataochljud.se>
2020-04-22 13:31:48 +0200

    Bitshift function
    Bitshifts graphics by 1,2,4 or 8 pixels.

---
+++ AtariSTgfx/bitshift.cpp
@@ -0,0 +1,8 @@
+
+// Bitshifting graphics
+
+#include <stdio.h>
+
+/* Bitshifts Graphics data by 1,2,4 or 8 pixels
+   returns the shifted graphics (pointer array)
+void[] * bitshift(int pixels, void * data, int data_size);
